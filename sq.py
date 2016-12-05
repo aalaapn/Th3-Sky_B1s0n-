f2b7040544ab49be2ca079961b18e2ca34f974df
Aalaap Narasipura <agn27@cornell.edu>
2016-12-04 17:55:44 -0800

    windows

---
+++ sq.py
@@ -0,0 +1,34 @@
+questions = []
+answers = []
+questions.append("""
+If I were a twin, would I still be me, or would I be the other one?
+    1. Me
+    2. Other one
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+At what elevation does the sky begin?
+    1. pi
+    2. e
+    3. e^pi
+    4. j^(2pi)
+    5. 10!
+""")
+answers.append(5)
+#####################################################################
+questions.append("""
+Is this frog for real?
+    1. yes
+    2. no
+""")
+answers.append(1)
+#####################################################################
+
+
+
+
+def get_questions():
+    return questions
+def get_answers():
+    return answers
