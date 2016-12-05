18660982be669bc4c2f7fbcd421a61e97b2e0656
Aalaap Narasipura <agn27@cornell.edu>
2016-12-04 22:28:39 -0500

    thanks jp

--- sq.py
+++ sq.py
@@ -24,6 +24,325 @@ Is this frog for real?
 """)
 answers.append(1)
 #####################################################################
+questions.append("""
+Why isn't palindrome a palindrome?
+    1. English sucks
+    2. the world hates nice things 
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+"Imagine the first time a baby sneezes... what the fuck was that."
+    1. i don't like babies
+    2. yes
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+So, like, you know how when you're doing laundry, and you'll see a sock on the floor and you throw it in the washer because, hey it's just one sock. One sock more can't hurt it. Does that mean if you wash all socks, you can fit an infinite amount of socks in the washer?
+    1. yes
+    2. socks are immutable
+    3. Washing machines are infinite
+    4. 8 sideways
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+I could replace the meat inside this burrito with potatoes, and then I can replace the veggies with nacho cheese, and more potatoes.
+    1. potato^2
+    2. potato 
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+How many badgers, on average, equal the weight of a horse?
+    1. 10
+    2. 20
+    3. 30
+    4. 40
+    5. 50
+    6. 60
+    7. 70
+    8. 80
+    9. right answer is 70
+""")
+answers.append(9)
+#####################################################################
+questions.append("""
+Do dogs have a concept of math? Like, can they see that they have two bones, but that other dog has three bones, and if he eats his bones he will have none?
+    1. Dogs invented multivariable calculus 
+    2. dogs == math
+    3. dog >> science  
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+Pineapple. Why apple?
+    1. apple pen 
+    2. pineapple pen
+    3. pen pineapple apple pen  
+""")
+answers.append(3)
+#####################################################################
+questions.append("""
+If it's called a bladder, what does it blad?
+    1. pee
+    2. urine
+    3. yellow water
+    4. snow cone topping  
+""")
+answers.append(4)
+#####################################################################
+questions.append("""
+I like cutting the grass. It's like im cutting the earth's hair
+    1. Good for you
+    2. good for me
+    3. good for us  
+""")
+answers.append(3)
+#####################################################################
+questions.append("""
+What Cornell alum would you most want to have a dink with?
+    1. Bill Nye 
+    2. Notorious RBG 
+    3. Irwin Jacobs
+    4. Christopher Reeve
+    5. E. B. White 
+    6. Kurt Vonnegut 
+    7. Bill Maher 
+    8. Glenn Pop Warner 
+    9. Andy Bernard 
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+How many bass drops would Wolfgang Amadeus Mozart deem acceptable?
+    1. 1
+    2. Zero
+    3. infinity
+    4. 42
+    5. 71 
+    6. 21
+    7. -1
+    8. j
+""")
+answers.append(8)
+#####################################################################
+questions.append("""
+When is the worst time to get sick?
+    1. Sunday
+    2. July 29 
+    3. Day after your birthday
+    4. Morgan Freeman's Birthday
+    5. the result of 3+7  
+    6. May?  
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+What species of whale is Kanye West?
+    1. Blue Whale
+    2. Great whale 
+    3. Killer Whale 
+    4. Fin Whale 
+    5. Humpback Whale 
+    6. Gray Whale
+    7. Sperm Whale 
+    8. Beluga Whale  
+""")
+answers.append(7)
+#####################################################################
+questions.append("""
+When is the worst time to get sick?
+    1. Sunday
+    2. July 29 
+    3. Day after your birthday
+    4. Morgan Freeman's Birthday
+    5. the result of 3+7  
+    6. May?  
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+What sound do people make when they're confused?
+    1. Ummmm
+    2. IDK
+    3. hmmm
+    4. what?
+    5. no sound 
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+What is the greatest band ever?
+    1. king gizzard and the lizard wizard
+    2. metz
+    3. kiss
+    4. The impala that is tame
+    5. the deceased Michael Jackson
+    6. WHO?
+    7. Coleopteras
+    8. DEATH grips  
+""")
+answers.append(6)
+#####################################################################
+questions.append("""
+Is the S or the C silent in scent?
+    1. S
+    2. C
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+If you get out of the shower clean then how does your towel get dirty?
+    1. Chupacabra
+    2. Aliens
+    3. Big Foot
+    4. Yeti
+    5. Vampires
+    6. Your shadow self
+""")
+answers.append(6)
+#####################################################################
+questions.append("""
+Do you think sand is called sand because it's between the sea and land?
+    1. yes
+    2. maybe
+    3. no
+    4. plead the 5th
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+If money is the root of all evil, why do they ask for it in church?
+    1. ^^^
+    2. church is evil?
+    3. there is no evil
+    4. it's all a simulation
+    5. string theory?
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+What does water taste like?
+    1. H2O
+    2. life
+    3. nothing
+    4. everything
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+If you drop soap on the floor is the floor clean or the soap dirty?
+    1. Floor is clean
+    2. Soap is dirty
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+How come our nose run but our feet smell
+    1. Illuminatii
+    2. Evolution
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+Are you telling the truth if you lie in bed?
+    1. Yes?
+    2. no.
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+If only 2 percent is milk what's the other 98%? 
+    1. shut up 
+    2. ur dumb
+    3. these are dumb questions 
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+Why is called a building when it's already build?
+    1. ?????
+    2. !!!!!!!
+    3. ....... 
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+Why are there more nipples than people?
+    1. nipples are more important than people
+    2. 2 is better than one
+    3. fish?
+    4. phish. 
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+Who knew what time it was when the first clock was made?
+    1. Ben Franklin 
+    2. Euler
+    3. Newton
+    4. Fourier 
+    5. Hawking
+    6. Gates
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+If two vegans are arguing is it still considered beef?
+    1. Yes
+    2. Yes^2
+    3. Yes^3
+    4. False. Humans are omnivore because we have canine teeth
+""")
+answers.append(3)
+#####################################################################
+questions.append("""
+If I weigh 99 pounds and eat a pound of nachos, am I 1% nacho?    
+    1. nacho says yes
+    2. no
+    3. plead the second
+""")
+answers.append(1)
+#####################################################################
+questions.append("""
+When you buy a bigger bed you have more bed room but less bedroom.  
+    1. Ahoy mate
+    2. indeed
+    3. yes
+    4. true
+    5. mind = blown
+""")
+answers.append(5)
+#####################################################################
+questions.append("""
+What color are mirrors
+    1. mirrors
+    2. reality
+    3. imagination
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+Why are there no B batteries?
+    1. there are dumbass
+    2. B-ism
+    3. A-sim
+    4. C-u-l8ter
+""")
+answers.append(2)
+#####################################################################
+questions.append("""
+If Nike is pronounced Nikee then why isn't bike pronounced bikee
+    1. fuck u
+    2. trikee
+    3. mikee
+    4. likee
+    5. yees
+""")
+answers.append(1)
+
 
 
 
